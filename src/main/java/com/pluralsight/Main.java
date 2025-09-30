f6a663d416ce259c01c261b9b0de865a5df34e5b
Mstimphil <mstimphil@my.yearupunited.org>
2025-09-30 10:41:18 -0400

    scanner message

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,18 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Please enter your name and date of the show(MM/dd/yy. How many tickets do you need? ");
+        String userName = "Arnold Jackson";
+
+
+
+
+
 
     }
 }

378edec86ed6ac7adf55138968ee6630e06566b1
Mstimphil <mstimphil@my.yearupunited.org>
2025-09-30 11:07:20 -0400

    date  message

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -6,8 +6,12 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner myScanner = new Scanner(System.in);
-        System.out.println("Please enter your name and date of the show(MM/dd/yy. How many tickets do you need? ");
-        String userName = "Arnold Jackson";
+        System.out.println("Please enter your name.");
+        String userName = myScanner.nextLine();
+        System.out.println("What is the date of the show \"MM/dd/yy\"?" );
+        String getDate = myScanner.nextLine();
+
+
 
 
 
